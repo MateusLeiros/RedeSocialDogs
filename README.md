46f149a0ad6d5f416ab6cc9f8475eb46025b89ac
Mateus Leiros <mateusleiros@gmail.com>
2023-07-13 15:58:10 -0300

    Projeto iniciado em react

--- README.md
+++
@@ -1,2 +0,0 @@
-# RedeSocialDogs
-Projeto criado para estudo de Javascript e React
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1>App React</h1>
+    <h3>Aqui começa o nosso projeto!</h3>
+      
+    </div>
+  )
+}
+
+export default App
